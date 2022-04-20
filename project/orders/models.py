5aaef152c4aa4ab100399dc76774f83b220c030c
muhammed seydali <60098100+muhammedseydali@users.noreply.github.com>
2022-04-20 12:13:45 +0530

    order models change

--- project/orders/models.py
+++ project/orders/models.py
@@ -35,6 +35,17 @@ class Payment(models.Model):
     def __str__(self) :
         return self.payment_id
 
+# class Payees(models.Model):
+#     user = models.ForeignKey(Account,on_delete=models.CASCADE)
+#     payment_id = models.CharField(max_length=100)
+#     payment_method = models.CharField(max_length=100)
+#     amount_paid = models.CharField(max_length=100)
+#     status = models.CharField(max_length=100)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self) :
+#         return self.payment_id        
+
 
 class   Order(models.Model):
     STATUS = (
